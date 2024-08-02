0ffdf7559cbb2249595ed856df96a73e831fffe7
andrei1508 <andrei_cna@yahoo.com>
2024-08-02 18:37:24 +0300

    Feature 2 Service

---
+++ app/Services/Feature2Service.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+
+class Feature2Service {
+
+    public function getFeature() {
+        //code
+    }
+
+    public function modify() {
+        //code
+    }
+}
