138778617d6278991c72964ec3ebadc6ed020c53
andrei1508 <andrei_cna@yahoo.com>
2024-08-02 18:38:18 +0300

    Feature 3 Service

---
+++ app/Services/Feature3Service.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+
+class Feature3Service {
+
+    public function getFeature() {
+        //code
+    }
+
+    public function modify() {
+        //code
+    }
+}
